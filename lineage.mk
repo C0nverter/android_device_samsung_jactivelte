43cf7b3881798cb7af1a2dc53ac0437383c5a31c
Vlad <tsvetkov_vlad@ukr.net>
2018-04-05 16:19:35 +0300

    jactivelte: Unbreak fingerprint
    after https://review.lineageos.org/#/c/208868/

--- lineage.mk
+++ lineage.mk
@@ -6,8 +6,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jactivelte \
     TARGET_DEVICE=jactivelte \
-    BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:5.0.1/LRX22C/I9295XXUDPF1:user/release-keys" \
     PRIVATE_BUILD_DESC="jactivelte-user 5.0.1 LRX22C I9295XXUDPF1 release-keys"
 
 PRODUCT_DEVICE := jactivelte
 PRODUCT_NAME := lineage_jactivelte
+BUILD_FINGERPRINT := samsung/jactivelte/jactivelte:5.0.1/LRX22C/I9295XXUDPF1:user/release-keys
